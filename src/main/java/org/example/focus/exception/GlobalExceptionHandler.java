228d1444c7869a1eb8570550d77da632e46df7a2
win9 <krrr883@gmail.com>
2024-06-17 00:15:13 +0900

    [feat] notExsit예외 추가

--- src/main/java/org/example/focus/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/focus/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.focus.common.BaseErrorResopnse;
 import org.example.focus.exception.exist.ExistException;
 import org.example.focus.exception.notFound.NotFoundException;
+import org.example.focus.exception.notexist.NotExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,6 +23,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<BaseErrorResopnse> handleNotFoundException(NotFoundException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new BaseErrorResopnse(e.getStatus(), e.getErrorMessage()),
+                HttpStatus.valueOf(e.getStatus()));
+    }
+
+    @ExceptionHandler(NotExistException.class)
+    public ResponseEntity<BaseErrorResopnse> handleNotFoundException(NotExistException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(new BaseErrorResopnse(e.getStatus(), e.getErrorMessage()),
                 HttpStatus.valueOf(e.getStatus()));
