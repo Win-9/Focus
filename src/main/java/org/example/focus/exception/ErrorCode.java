6e427d1ee76b1e9520c48bdced601c153975db1b
win9 <krrr883@gmail.com>
2024-06-17 00:07:03 +0900

    [feat] 정책 변경으로 인한 에러메시지 한글로 변경

--- src/main/java/org/example/focus/exception/ErrorCode.java
+++ src/main/java/org/example/focus/exception/ErrorCode.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    EXIST_BOOK(HttpStatus.BAD_REQUEST, "Book is already exsit"),
-    BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "Not exist book");
+    EXIST_BOOK(HttpStatus.BAD_REQUEST, "이미 존재하는 책 입니다."),
+    BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 책 입니다.");
 
     private HttpStatus httpStatus;
     private String message;
