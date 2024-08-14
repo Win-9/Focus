e917583906a906353e64859834a45020fe8acc6c
win9 <krrr883@gmail.com>
2024-08-15 00:27:42 +0900

    [feat] exceptionHandler validation 에러응답 halder 로직 구현

--- src/main/java/org/example/focus/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/focus/exception/GlobalExceptionHandler.java
@@ -7,12 +7,24 @@ import org.example.focus.exception.notFound.NotFoundException;
 import org.example.focus.exception.notexist.NotExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseErrorResopnse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+        BindingResult bindingResult = e.getBindingResult();
+        return new ResponseEntity<>(new BaseErrorResopnse(
+                HttpStatus.BAD_REQUEST.value(),
+                bindingResult.getFieldErrors().get(0).getDefaultMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(ExistException.class)
     public ResponseEntity<BaseErrorResopnse> handleNotExistException(ExistException e) {
