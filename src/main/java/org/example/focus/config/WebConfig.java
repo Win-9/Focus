6982f547c6a9e8d87e5ac60141104ee8abeceb12
win9 <krrr883@gmail.com>
2024-10-09 22:01:46 +0900

    [feat] 응답 samesite 적용 인터셉터 추가

--- src/main/java/org/example/focus/config/WebConfig.java
+++ src/main/java/org/example/focus/config/WebConfig.java
@@ -1,16 +1,18 @@
 package org.example.focus.config;
 
+import org.example.focus.interceptor.CookieInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CookieInterceptor())
+                .order(1)
+                .addPathPatterns("/**");
     }
 }
